28188342d3a9ef9c777b4570c1fbc18c2d19be84
Alexandre <95387509+afolgierini@users.noreply.github.com>
2022-10-20 19:14:18 -0300

    Update render.js

--- src/render.js
+++ src/render.js
@@ -1,26 +1,33 @@
-import {renderHome} from './home'
-import {renderMenu} from './menu'
-import {renderContact} from './contact'
+import { renderDefault } from './default'
+import { renderHome } from './home'
+import { renderMenu } from './menu'
+import { renderContact } from './contact'
 
 export const render = () => {    
-    document.querySelector('.home').addEventListener('mouseup', e => {
-        document.querySelector('.main').innerHTML = renderHome();
-        document.querySelector('.main-btn').addEventListener('mouseup', e => {
-            document.querySelector('.main').innerHTML = renderMenu()
+    const addEvents = () => {
+        document.querySelector('.home').addEventListener('mouseup', e => {
+            document.querySelector('.main').innerHTML = renderHome();
+            document.querySelector('.main-btn').addEventListener('mouseup', e => {
+                document.querySelector('.main').innerHTML = renderMenu();
+
+            })
         })
-    })
+    
+        document.querySelector('.menu').addEventListener('mouseup', e => {
+            document.querySelector('.main').innerHTML = renderMenu();
+        })
+    
+        document.querySelector('.main-btn').addEventListener('mouseup', e => {
+            document.querySelector('.main').innerHTML = renderMenu();
+        })
+    
+        document.querySelector('.contact').addEventListener('mouseup', e => {
+            document.querySelector('.main').innerHTML = renderContact();
+        })
+    }
 
-    document.querySelector('.menu').addEventListener('mouseup', e => {
-        document.querySelector('.main').innerHTML = renderMenu()
-    })
 
-    document.querySelector('.main-btn').addEventListener('mouseup', e => {
-        document.querySelector('.main').innerHTML = renderMenu()
-    })
-
-    document.querySelector('.contact').addEventListener('mouseup', e => {
-        document.querySelector('.main').innerHTML = renderContact()
-    })
-
-    document.querySelector('.year').innerHTML = new Date().getFullYear()
+    document.querySelector('#app').innerHTML = renderDefault();
+    document.querySelector('.main').innerHTML = renderHome();
+    addEvents()
 }
