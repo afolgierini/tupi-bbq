a0969756eb330529bf19a7fda0a4395aae5a6171
Alexandre <alexandreabef@gmail.com>
2022-10-20 22:40:11 -0300

    dist push for github pages

--- assets/index.6243ea6c.js
+++ assets/index.6243ea6c.js
@@ -1,16 +1,38 @@
-(function(){const c=document.createElement("link").relList;if(c&&c.supports&&c.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))d(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&d(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function d(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const n="/tupi-bbq/assets/img-home.545bd9d8.jpg",l=()=>`
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function d(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=d(e);fetch(e.href,t)}})();let l=new Date().getFullYear();const m=()=>`
+        <div class="default">
+            <div class="header">
+                <div class="logo">
+                    <p class="logoName">Tupi BBQ</p>
+                </div>
+
+                <div class="navOptions">
+                <div class="options home">Home</div>
+                <div class="options menu">Menu</div>
+                <div class="options contact">Contact</div>
+                </div>
+            </div>
+
+            <div class="main">
+                
+            </div>
+
+            <div class="footer">
+                <p>\xA9 Tupi BBQ <p class="year">${l}</p></p>
+            </div>
+        </div>
+   `,p="/tupi-bbq/assets/img-home.545bd9d8.jpg",o=()=>`
     <div class="main-home">
         <p class="main-title">Brazilian barbecue as it's best!</p>
-        <img class="main-img" src="${n}" alt="">
+        <img class="main-img" src="${p}" alt="">
         <p class="main-description">Try now the incredible taste of the traditional Brazilian barbecue without a flight to Brazil.</p>
         <button class="main-btn">ORDER NOW</button>
     </div>
-`,m="/tupi-bbq/assets/picanha.877a0d7b.jpg",p="/tupi-bbq/assets/chicken.12afcbb9.jpg",u="/tupi-bbq/assets/sausage.0ae53c7a.jpg",v="/tupi-bbq/assets/ribs.a8cbc9e5.jpg",g="/tupi-bbq/assets/chicken-heart.a66e3e0b.jpg",b="/tupi-bbq/assets/kafta.23d534de.jpg",f="/tupi-bbq/assets/garlic-bread.acc73ae0.jpg",h="/tupi-bbq/assets/vinagrete.8be1918b.jpg",i="/tupi-bbq/assets/add-to-cart-icon.8d12f6e7.svg",r=()=>`
+`,u="/tupi-bbq/assets/picanha.877a0d7b.jpg",v="/tupi-bbq/assets/chicken.12afcbb9.jpg",g="/tupi-bbq/assets/sausage.0ae53c7a.jpg",b="/tupi-bbq/assets/ribs.a8cbc9e5.jpg",f="/tupi-bbq/assets/chicken-heart.a66e3e0b.jpg",h="/tupi-bbq/assets/kafta.23d534de.jpg",y="/tupi-bbq/assets/garlic-bread.acc73ae0.jpg",q="/tupi-bbq/assets/vinagrete.8be1918b.jpg",i="/tupi-bbq/assets/add-to-cart-icon.8d12f6e7.svg",c=()=>`
         <p class="menu-title">Menu</p>
         <div class="main-menu">
             <div class="card">
                 <div class="card-content">
-                    <img src="${m}" alt="">
+                    <img src="${u}" alt="">
                 </div>
                 <div class="card-content-right">
                     <p class="card-title">Rump Cap</p>
@@ -22,7 +44,7 @@
             </div>
             <div class="card">
                 <div class="card-content">
-                <img src="${p}" alt="">
+                <img src="${v}" alt="">
                 </div>
                 <div class="card-content-right">
                 <p class="card-title">Chicken</p>
@@ -34,7 +56,7 @@
             </div>
             <div class="card">
                 <div class="card-content">
-                <img src="${u}" alt="">
+                <img src="${g}" alt="">
                 </div>
                 <div class="card-content-right">
                 <p class="card-title">Sausage</p>
@@ -46,7 +68,7 @@
             </div>
             <div class="card">
                 <div class="card-content">
-                <img src="${v}" alt="">
+                <img src="${b}" alt="">
                 </div>
                 <div class="card-content-right">
                 <p class="card-title">Ribs</p>
@@ -58,7 +80,7 @@
             </div>
             <div class="card">
                 <div class="card-content">
-                <img src="${g}" alt="">
+                <img src="${f}" alt="">
                 </div>
                 <div class="card-content-right">
                 <p class="card-title">Chicken Heart</p>
@@ -70,7 +92,7 @@
             </div>
             <div class="card">
                 <div class="card-content">
-                <img src="${b}" alt="">
+                <img src="${h}" alt="">
                 </div>
                 <div class="card-content-right">
                 <p class="card-title">Kafta</p>
@@ -82,7 +104,7 @@
             </div>
             <div class="card">
                 <div class="card-content">
-                <img src="${f}" alt="">
+                <img src="${y}" alt="">
                 </div>
                 <div class="card-content-right">
                 <p class="card-title">Garlic Bread</p>
@@ -94,7 +116,7 @@
             </div>
             <div class="card">
                 <div class="card-content">
-                <img src="${h}" alt="">
+                <img src="${q}" alt="">
                 </div>
                 <div class="card-content-right">
                 <p class="card-title">Vinagrete</p>
@@ -105,16 +127,16 @@
                 </div> 
             </div> 
         </div>
-    `,y="/tupi-bbq/assets/location.345926d8.svg",q="/tupi-bbq/assets/phone.6d718f1f.svg",L=()=>`
+    `,L="/tupi-bbq/assets/location.345926d8.svg",$="/tupi-bbq/assets/phone.6d718f1f.svg",S=()=>`
       <p class="contact-title">Contact</p>
       <div class="main-contact">
          <div class="info">
             <div class="location">
-               <img class="svg" src="${y}" alt="">  
+               <img class="svg" src="${L}" alt="">  
                <p>You can find us almost everywhere in the world!</p>                     
             </div>
             <div class="email">
-               <img src="${q}" alt="">
+               <img src="${$}" alt="">
                <p>Check now in your local food delivery apps.</p>
             </div>
          </div>
@@ -140,4 +162,4 @@
             </form>
          </div>
       </div> 
-   `,$=()=>{document.querySelector(".home").addEventListener("mouseup",s=>{document.querySelector(".main").innerHTML=l(),document.querySelector(".main-btn").addEventListener("mouseup",c=>{document.querySelector(".main").innerHTML=r()})}),document.querySelector(".menu").addEventListener("mouseup",s=>{document.querySelector(".main").innerHTML=r()}),document.querySelector(".main-btn").addEventListener("mouseup",s=>{document.querySelector(".main").innerHTML=r()}),document.querySelector(".contact").addEventListener("mouseup",s=>{document.querySelector(".main").innerHTML=L()}),document.querySelector(".year").innerHTML=new Date().getFullYear()};console.log(n);$();
+   `,H=()=>{const r=()=>{document.querySelector(".home").addEventListener("mouseup",s=>{document.querySelector(".main").innerHTML=o(),document.querySelector(".main-btn").addEventListener("mouseup",d=>{document.querySelector(".main").innerHTML=c()})}),document.querySelector(".menu").addEventListener("mouseup",s=>{document.querySelector(".main").innerHTML=c()}),document.querySelector(".main-btn").addEventListener("mouseup",s=>{document.querySelector(".main").innerHTML=c()}),document.querySelector(".contact").addEventListener("mouseup",s=>{document.querySelector(".main").innerHTML=S()})};document.querySelector("#app").innerHTML=m(),document.querySelector(".main").innerHTML=o(),r()};H();
